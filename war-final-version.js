a84752130dffe54c920c552cdfa9a7af57d607dd
RKVanHorn <acinonyx79@hotmail.com>
2023-05-12 18:53:59 -0700

    include v2 and new final version and remove old html file

--- war-final-version.js
+++ war-final-version.js
@@ -3,8 +3,8 @@ class Deck {
         this.deck = [];
         
     }
-/*create a deck that consists of an array of 52 objects that each have 2
-key:value pairs consisting of name and value. Also, Aces high!*/ 
+    /*create a deck that consists of an array of 52 objects that each have 2
+    key:value pairs consisting of name and value. Also, Aces high!*/ 
     createDeck(){
         const suits = ['♥', '♦', '♣', '♠'];
         const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
@@ -12,15 +12,15 @@ key:value pairs consisting of name and value. Also, Aces high!*/
             for (let cardValue of values){
                 this.deck.push({
                     name: `${cardValue} ${suitName}`,
-                    value: values.indexOf(cardValue)
+                    value: values.indexOf(cardValue) + 2
                 });
             }
         }
         //console.log(this.deck);
 
     }
-/* Shuffle the deck swapping the card at index i with a card at another index
- chosen randomly by math.random*/
+    /* Shuffle the deck swapping the card at index i with a card at another index
+    chosen randomly by math.random (The Fisher Yates method)*/
     shuffleDeck(){
         for(let i = 0; i < this.deck.length; i++){
             let tempCard = this.deck[i];
@@ -30,101 +30,102 @@ key:value pairs consisting of name and value. Also, Aces high!*/
         } 
         //console.log(this.deck);
     }  
+
+    /* I wanted to create a method that contained the 2 methods from the 
+    Deck class, trying to simplify what my playRound function would have in it*/
+    setUp(){
+        this.createDeck();        
+        this.shuffleDeck();
+    
+    }
     
 }
 
-class Players {
+class Player {
+    constructor(){        
+        this.playerHand = [];
+        this.playerScore = 0;
+    }   
+    
+}
+
+class Game{
     constructor(){
-        this.playerDeck = new Deck;
-        this.player1Hand = [];
-        this.player2Hand = [];
-        this.player1Score = 0;
-        this.player2Score = 0;
-    }
-    /* I wanted to create a method that contained the 2 methods from the 
-    Deck class, that way I would only need to instantiate the Player class inside
-    the Game class and not the Player and Deck classes*/
-    setUp(){
-        this.playerDeck.createDeck();        
-        this.playerDeck.shuffleDeck();
-        
-    }
+        this.gamePlayer1 = new Player
+        this.gamePlayer2 = new Player
+        this.gameDeck = new Deck;
+       // console.log(this.gamePlayer1);
+        //console.log(this.gamePlayer2);
+        //console.log(this.gameDeck);
+    }  
 
     /* deal 26 cards to each of the players by using a loop and for each iteration,
     popping the top card off the deck and pushing it into player 1's hand and 
     then doing the same for player2. */
     dealHand(){
         for(let i = 0; i < 26; i++){
-            this.player1Hand.push(this.playerDeck.deck.pop());
-            this.player2Hand.push(this.playerDeck.deck.pop());
+        this.gamePlayer1.playerHand.push(this.gameDeck.deck.pop());
+        this.gamePlayer2.playerHand.push(this.gameDeck.deck.pop());
         }
-        //console.log(this.player1Hand);
-        //console.log(this.player2Hand);
+        //console.log(this.gamePlayer1.playerHand);
+        //console.log(this.gamePlayer2.playerHand);
     }
-    
-    
-}
 
-class Game{
-    constructor(){
-        this.gamePlayer = new Players
-    }
-/* play a round of War by removing one card from each players hand and comparing the
-values of those cards. Value comes from the key:value pairs created in the Deck class.
-Depending on who has the higher card value for each round a point is awarded. In the case of a 
-tie, no points are awarded and the score remains the same.*/
+
+    /* play a round of War by removing one card from each players hand and comparing the
+    values of those cards. Value comes from the key:value pairs created in the Deck class.
+    Depending on who has the higher card value for each round a point is awarded. In the case of a 
+    tie, no points are awarded and the score remains the same.*/
     playRound(){    
         let player1Card  
         let player2Card 
         console.log(`Shall we play a game?`)
         for(let i = 0; i < 26; i++){
-             player1Card = this.gamePlayer.player1Hand.pop();
-             player2Card = this.gamePlayer.player2Hand.pop();
+             player1Card = this.gamePlayer1.playerHand.pop();
+             player2Card = this.gamePlayer2.playerHand.pop();
             console.log(`Player 1 plays: ${player1Card.name}` + "\n" +
                         `Player 2 plays: ${player2Card.name}`)
             if(player1Card.value > player2Card.value){
-                this.gamePlayer.player1Score += 1;
-                //console.log(`The current score is P1: ${player1Score} P2: ${player2Score}`)
+                this.gamePlayer1.playerScore += 1;                
             } else if (player2Card.value > player1Card.value){
-                this.gamePlayer.player2Score +=1;
-                //console.log(`The current score is P1: ${player1Score} P2: ${player2Score}`)
+                this.gamePlayer2.playerScore +=1;                
             } else {
-                this.gamePlayer.player1Score +=0;
-                this.gamePlayer.player2Score +=0;
+                this.gamePlayer1.playerScore +=0;
+                this.gamePlayer2.playerScore +=0;
                 
             }
-            console.log(`The current score is P1: ${this.gamePlayer.player1Score} P2: ${this.gamePlayer.player2Score}`)
+            console.log(`The current score is P1: ${this.gamePlayer1.playerScore} P2: ${this.gamePlayer2.playerScore}`)
             
         }
-        console.log(`The final score is P1: ${this.gamePlayer.player1Score} P2: ${this.gamePlayer.player2Score}`)
+        console.log(`The final score is P1: ${this.gamePlayer1.playerScore} P2: ${this.gamePlayer2.playerScore}`)
     }
     /* Using the scores generated in the playRound method, declare a winner by determining
     who has the higher score or declare a tie in the case that neither side has a higher score.*/
     declareWinner(){
-        if(this.gamePlayer.player1Score > this.gamePlayer.player2Score){
+        if(this.gamePlayer1.playerScore > this.gamePlayer2.playerScore){
             console.log(`Congratulations Player 1, YOU WIN!`);
-        } else if (this.gamePlayer.player2Score > this.gamePlayer.player1Score){
+        } else if (this.gamePlayer2.playerScore > this.gamePlayer1.playerScore){
             console.log(`Congratulations Player 2, YOU WIN!`);
         } else {
             console.log(`It's a tie! Now, how about a nice game of chess?`);
         }
     }
-/* put all the methods together so that only one needs to be called to run a game.*/
+    /* put all the methods together so that only one needs to be called to run a game.*/
     playGame(){
-        this.gamePlayer.setUp();
-        this.gamePlayer.dealHand();
-        this.playRound(this.player1Hand, this.player2Hand);
-        this.declareWinner(this.player1Score, this.player2Score);  
+        this.gameDeck.setUp();
+        this.dealHand();
+        this.playRound(this.gamePlayer1.playerHand, this.gamePlayer2.playerHand);
+        this.declareWinner(this.gamePlayer1.playerScore, this.gamePlayer2.playerScore);  
 
     }
 
 }
 
 let test = new Game;
-//test.setUp();
-//test.dealHand();
-//test.playRound();
-//test.declareWinner();
+// test.gameDeck.setUp();
+// console.log(test.gameDeck);
+// test.dealHand();
+
+// test.playRound();
+// test.declareWinner();
 test.playGame();
-
-
